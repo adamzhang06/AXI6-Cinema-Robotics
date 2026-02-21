8d4f099ebd23c5a72fec125c1b7147e8f38c5ede
Adam Zhang <reachadamzhang@gmail.com>
2026-02-20 23:46:22 -0500

    make cam 1080p

--- pi/haarcascade.py
+++ pi/haarcascade.py
@@ -9,8 +9,8 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 def generate_frames():
     cap = cv2.VideoCapture(0)
     # Keeping resolution low for Pi 3B performance
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     while True:
         success, frame = cap.read()
