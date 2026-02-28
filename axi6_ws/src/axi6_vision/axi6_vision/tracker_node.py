99c7c8b8bc1d52c9ecfe7152f21c2240f89bbd1b
Adam Zhang <reachadamzhang@gmail.com>
2026-02-27 19:15:22 -0500

    update to tracker_node.py

--- axi6_ws/src/axi6_vision/axi6_vision/tracker_node.py
+++ axi6_ws/src/axi6_vision/axi6_vision/tracker_node.py
@@ -7,7 +7,6 @@ class TrackerNode(Node):
     def __init__(self):
         super().__init__("tracker_node")
  
- 
 def main(args=None):
     rclpy.init(args=args)
     node = TrackerNode()
@@ -22,6 +21,21 @@ def main(args=None):
         if rclpy.ok():
             rclpy.shutdown()
  
+def vision(args=model_path):
+    rclpy.init(args=args)
+    node = TrackerNode()
+    node.get_logger().info("Starting camera vision...")
+    
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt (SIGINT) received. Shutting down...")
+    finally:
+        # This ensuring the node cleans up its network connections
+        if rclpy.ok():
+            rclpy.shutdown()
+
  
 if __name__ == "__main__":
     main()
+    vision()
